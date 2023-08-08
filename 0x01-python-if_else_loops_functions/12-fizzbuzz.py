1b15f68828743491442ec1cb18ed61122ec45bc1
Rophpad <rophenp@gmail.com>
2023-08-08 07:52:54 -0700

    add spaces after keyword: elif

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,9 +3,9 @@ def fizzbuzz():
     for number in range(1, 101):
         if (number % 3 == 0 and number % 5 != 0):
             print("Fizz", end=' ')
-        elif(number % 5 == 0 and number % 3 != 0):
+        elif (number % 5 == 0 and number % 3 != 0):
             print("Buzz", end=' ')
-        elif(number % 15 == 0):
+        elif (number % 15 == 0):
             print("FizzBuzz", end=' ')
         else:
             print("{}".format(number), end=' ')
