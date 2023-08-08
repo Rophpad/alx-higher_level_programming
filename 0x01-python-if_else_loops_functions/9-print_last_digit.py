f0f8bfd8c08c950ec8f7e3fc884016cb37885334
Rophpad <rophenp@gmail.com>
2023-08-08 07:02:45 -0700

    Handle negative number caase

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    return (number % 10)
+    if (number < 0):
+        lastDigit = (-number) % 10
+    else:
+        lastDigit = number % 10
+    return lastDigit
