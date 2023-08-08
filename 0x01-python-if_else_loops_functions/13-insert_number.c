16bcec59dc191639c84ee49fd668175c58948898
Rophpad <rophenp@gmail.com>
2023-08-08 09:11:46 -0700

    13-insert_number.c

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -23,8 +23,8 @@ listint_t *insert_node(listint_t **head, int number)
 	}
 	while (node && node->next && node->next->n < number)
 		node = node->next;
-	new->next = node->next;
-	node->next = new;
+	newNode->next = node->next;
+	node->next = newNode;
 
 	return (new);
 }
