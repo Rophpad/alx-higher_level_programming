420cf4bddf3a2f49eb4db19577ac9b227c689bd9
Rophpad <rophenp@gmail.com>
2023-08-13 12:48:38 -0700

    code update

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if len(matrix) == 1 and len(matrix[0] == 0):
+    if len(matrix) == 1 and len(matrix[0]) == 0:
         print("".format())
     for i in range(len(matrix)):
         idx = 0
         for element in matrix[i]:
-            if (idx < len(matrix[i] - 1)):
+            if (idx < len(matrix[i]) - 1):
                 print("{}".format(element), end=' ')
             else:
                 print("{}".format(element), end='\n')
-                idx += 1
+            idx += 1
