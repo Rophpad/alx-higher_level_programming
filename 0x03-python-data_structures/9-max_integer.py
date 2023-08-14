6e4ad2f683c31dd854ff7861a20c023026ae2a07
Rophpad <rophenp@gmail.com>
2023-08-14 02:14:58 -0700

    code update

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max_int = 0
     if len(my_list) == 0:
         return None
     else:
-        for element in my_list:
+        max_int = my_list[0]
+        for element in my_list[1:]:
             if max_int < element:
                 max_int = element
         return max_int
