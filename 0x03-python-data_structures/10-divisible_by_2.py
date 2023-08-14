a875d9e960db9a51e9078444c3561d51fb3ba206
Rophpad <rophenp@gmail.com>
2023-08-14 02:55:35 -0700

    code update

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     bool_list = []
     for element in my_list:
-        if not(element % 2):
+        if (element % 2 == 0):
             bool_list.append(True)
         else:
             bool_list.append(False)
