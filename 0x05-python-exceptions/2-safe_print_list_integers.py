e6d59d515c9c96e42205db31083af04691468170
Rophpad <rophenp@gmail.com>
2023-08-28 16:13:22 +0000

    Print and count integers

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    i = not_int = 0
+    while i < x:
+        try:
+            print("{:d}".format(my_list[i]), end='')
+            i += 1
+            if i == x:
+                print('')
+                return i
+        except (TypeError, ValueError)
+            i += 1
+            not_int += 1
+            if i == x
+                print('')
+                return not_int
