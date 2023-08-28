0adf1a823fe412f565d64a0db0542effa9ae07df
Rophpad <rophenp@gmail.com>
2023-08-28 16:00:49 +0000

    removed comment

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,8 +5,6 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[i]), end='')
             i += 1
-            # if i == x:
-                # raise IndexError
         except IndexError:
             break
     print('')
