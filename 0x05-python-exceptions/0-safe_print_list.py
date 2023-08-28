e2f32ac9932b9dffc2e8c0a77483bf6bdd1e8e6d
Rophpad <rophenp@gmail.com>
2023-08-28 15:56:10 +0000

    used str.format and code update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     i = 0
-    while True:
-        if i < x:
-            try:
-                print(my_list[i], end='')
-                i += 1
-                if i == x:
-                    raise IndexError
-            except IndexError:
-                print('')
-                print("nb_print: {}".format(i))
-                break
-        else:
-            print("nb_print: {}".format(i))
+    while i < x:
+        try:
+            print("{}".format(my_list[i]), end='')
+            i += 1
+            if i == x:
+                raise IndexError
+        except IndexError:
+            print('')
             break
+    return i
