f9c8f27287e1f56acf92c7ed148b14a256c95cef
Rophpad <rophenp@gmail.com>
2023-09-04 18:59:09 +0000

    Fix: [syntax error]

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -19,13 +19,13 @@ class Rectangle:
             """ Get the width of the rectangle """
             return self.__width
 
-        @wigth.setter
+        @width.setter
         def width(self, value):
             """ Set the value of width """
-            if type(value) not int:
-                raise: TypeError("width must be an integer")
+            if type(value) is not int:
+                raise TypeError("width must be an integer")
             if value < 0:
-                raise: ValueError("width must be >= 0")
+                raise ValueError("width must be >= 0")
             self.__width = value
 
             @property
