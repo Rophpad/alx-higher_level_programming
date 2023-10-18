083c5adc130b47b663939559e54d50daa4c225f2
Rophpad <rophenp@gmail.com>
2023-10-18 21:56:23 +0100

    Code syntax improved

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,7 +1,8 @@
 -- Creates the MySQL server user user_0d_1.
 CREATE USER
-    IF NOT EXISTS `user_0d_1`@`localhost`
+    IF NOT EXISTS user_0d_1@localhost
     IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES
-    ON *.* TO 'user_0d_1'@'loclhost';
+    ON *.*
+    TO 'user_0d_1'@'loclhost';
 FLUSH PRIVILEGES;
