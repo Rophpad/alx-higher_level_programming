dd1518c7d5ac0dde42e2ccd3a5fe26a3cab6065c
Rophpad <rophenp@gmail.com>
2023-11-13 15:42:11 +0100

    brackets deleted

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -3,4 +3,4 @@
  * Script that prints the first
  * argument passed to it
  */
-console.log(typeof(process.argv[2]) === 'undefined' ? 'No argument' : process.argv[2]);
+console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
