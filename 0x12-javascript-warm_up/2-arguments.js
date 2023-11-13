c745540e1d895cb31ef574a9b29eda4ebd5215c7
Rophpad <rophenp@gmail.com>
2023-11-13 15:09:58 +0100

    a script that prints a message depending of the number of arguments passed

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/*
+ * Script that prints a message depending
+ * of the number of the arguments passed
+ */
+const process = require('process'); 
+let args = process.argv;
+if (args.length === 2) {
+	console.log('No argument');
+} else if (args.length === 3) {
+	console.log('Argument found');
+} else {
+	console.log('Arguments found');
+}
