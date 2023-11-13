db628ea852776983596475a2c794607f463ad3da
Rophpad <rophenp@gmail.com>
2023-11-13 15:17:01 +0100

    curly brace removed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -5,10 +5,7 @@
  */
 const process = require('process'); 
 let args = process.argv;
-if (args.length === 2) {
-	console.log('No argument');
-} else if (args.length === 3) {
-	console.log('Argument found');
-} else {
-	console.log('Arguments found');
-}
+if (args.length === 2) console.log('No argument');
+else if (args.length === 3) console.log('Argument found');
+else console.log('Arguments found');
+
