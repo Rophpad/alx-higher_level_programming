142631f59358e09bedad844db668876f8a8b4ec1
Rophpad <rophenp@gmail.com>
2023-11-17 22:10:29 +0100

    Filter states bu user input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Displays all values in the states table of hbtn_Oe_usa
+   where name matches the given argument
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM `states` ORDER BY `id`")
+    [print(state) for state in cur.fetchall() if state[1] == sys.argv[4]]
